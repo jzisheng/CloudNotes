d02e5bc87921ff8f3017ed23dbedc419d441b398
Jason Zisheng Chang <chang.j@live.com>
2017-03-31 20:09:21 +0000

    iOS Application set up

---
+++ iOS Application/CloudNotes-iOS/Pods/Target Support Files/Pods-Cloud Notes/Pods-Cloud Notes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cloud_Notes : NSObject
+@end
+@implementation PodsDummy_Pods_Cloud_Notes
+@end
--- iOS Application/CloudNotes-iOS/Pods/Target Support Files/Pods-Cloud Notes/Pods-Cloud Notes-umbrella.h
+++ iOS Application/CloudNotes-iOS/Pods/Target Support Files/Pods-Cloud Notes/Pods-Cloud Notes-umbrella.h
@@ -10,3 +10,7 @@
 #endif
 #endif
 
+
+FOUNDATION_EXPORT double Pods_Cloud_NotesVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Cloud_NotesVersionString[];
+
